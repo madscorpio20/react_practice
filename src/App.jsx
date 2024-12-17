3a9d98d5458f053586523a6ec08259d861fa341a
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2024-12-18 01:36:17 +0530

    hardcoded json added

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const data = {
+    
+  }
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+function Todo(props){
+  return (
+    <>
+      <h3>{props.item}</h3>
+      <p>{props.description}</p>
+    </>
+  )
+}
+
+export default App
